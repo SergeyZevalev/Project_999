05535801724b1ef8c9b4d29ec85a57625537ce87
Sergey Zevalev <sergeizevalyov@yandex.ru>
2024-03-15 15:18:28 +0300

    coroutines

--- app/src/main/java/com/example/project_999/core/Representative.kt
+++ app/src/main/java/com/example/project_999/core/Representative.kt
@@ -2,10 +2,7 @@ package com.example.project_999.core
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 interface Representative<T : Any> {
 
@@ -32,38 +29,3 @@ interface Representative<T : Any> {
 
 }
 
-interface RunAsync{
-
-    fun <T: Any> runAsync(
-        scope: CoroutineScope,
-        backgroundBlock: suspend () -> T,
-        uiBlock: (T) -> Unit
-    )
-
-    fun clear()
-
-    class Base(
-        private val dispatchersList: DispatchersList
-    ) : RunAsync{
-
-        private var job: Job? = null
-        override fun <T : Any> runAsync(
-            scope: CoroutineScope,
-            backgroundBlock: suspend () -> T,
-            uiBlock: (T) -> Unit
-        ) {
-            job = scope.launch(dispatchersList.background()) {
-                val result = backgroundBlock.invoke()
-                withContext(dispatchersList.ui()) {
-                    uiBlock.invoke(result)
-                }
-            }
-        }
-
-        override fun clear() {
-            job?.cancel()
-            job = null
-        }
-
-    }
-}
